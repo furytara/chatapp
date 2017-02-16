965a1d856d2531a89d240e797b864c38bfe8535b
Doanh <doanhhnqt74@gmail.com>
2017-02-16 18:20:20 +0700

    add views

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express'),
-	app = express();
+	app = express(),
+	path = require('path')
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('hogan-express'));
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.route('/').get(function(req, res, next){
-	res.send('<h1>Hello World</h1>');
+	res.send('index', {});
 })
 
 app.listen(3000, function(){
