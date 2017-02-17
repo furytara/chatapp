593dcda18d7bc592d56199aecc1ad2b56636f204
Doanh <doanhhnqt74@gmail.com>
2017-02-16 23:42:39 -0800

    add routes

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var express = require('express'),
-	app = express(),
-	path = require('path')
+    app = express(),
+    path = require('path')
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.route('/').get(function(req, res, next){
-	res.send('index', {});
-})
+require('./routes/routes.js')(express, app);
 
-app.listen(3000, function(){
-	console.log('CHAT APP working on port 3000');
+// app.route('/').get(function(req, res, next) {
+//     // res.send('<h1>Hello World!</h1>');
+//     res.render('index', {});
+// })
+
+app.listen(3000, function() {
+    console.log('CHAT APP working on port 3000');
 })
