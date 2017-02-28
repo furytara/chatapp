abd7fcebbaea1c58ac15554e622ab5519375e9d8
Doanh <doanhhnqt74@gmail.com>
2017-02-28 09:41:18 -0800

    log in by facebook

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
 var express = require('express'),
     app = express(),
-    path = require('path')
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    config = require('./config/config.js'),
+    ConnectMongo = require('connect-mongo')(session),
+    mongoose = require('mongoose').connect(config.dbURL),
+    passport = require('passport'),
+    FacebookStrategy = require('passport-facebook').Strategy;
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
-require('./routes/routes.js')(express, app);
+var env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+    //dev specific setting
+    app.use(session({ secret: config.sessionSecret, saveUninitialized: true, resave: true }));
+} else {
+    //enviroment setting
+    app.use(session({
+        secret: config.sessionSecret,
+        saveUninitialized: true,
+        resave: true,
+        store: new ConnectMongo({
+            // url: config.dbURL,
+            mongoose_connection: mongoose.connections[0],
+            stringify: true
+        })
+    }));
+}
 
-// app.route('/').get(function(req, res, next) {
-//     // res.send('<h1>Hello World!</h1>');
-//     res.render('index', {});
-// })
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/routes.js')(express, app, passport);
+
+require('./auth/passportAuth.js')(passport, FacebookStrategy, config, mongoose);
 
 app.listen(3000, function() {
     console.log('CHAT APP working on port 3000');
+    console.log('ENV: ' + env);
 })
--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,33 @@
-module.exports = function(express, app) {
+module.exports = function(express, app, passport) {
     var router = express.Router();
 
     router.get('/', function(req, res, next) {
         res.render('index', { title: 'Welcome to Chat App' });
     })
 
-    router.get('/chatrooms', function(req, res, next) {
-        res.render('chatrooms', { title: 'Chatrooms' });
+    function securePages(req, res, next) {
+        if (req.isAuthenticated()) {
+            next();
+        } else {
+            res.redirect('/');
+        }
+    }
+
+    router.get('/auth/facebook', passport.authenticate('facebook'));
+
+    router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+        failureRedirect: '/',
+        successRedirect: '/chatrooms'
+    }));
+
+    router.get('/chatrooms', securePages, function(req, res, next) {
+        res.render('chatrooms', { title: 'Chatrooms', user: req.user });
     })
+
+    router.get('/logout', function(req, res, next) {
+        req.logout();
+        res.redirect('/');
+    });
 
     app.use('/', router);
 }
